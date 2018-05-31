b8c2b3bd1980afd2f6d92c004de9b7ffb1a44368
Abdulkadir <ethiogentelman@gmail.com>
2018-05-31 19:25:34 -0400

    completed currency conversion-Task-2

--- src/components/CalcPayment.js
+++ src/components/CalcPayment.js
@@ -7,7 +7,8 @@ class Payment extends Component {
     super()
     this.state = {
       selectedCurrency: 'USD',
-      amount: 0,
+			amount: 0,
+			input:0
     };
   }
 
@@ -18,22 +19,41 @@ class Payment extends Component {
 		});
 	};
 
+	handleChange=(event) =>{
+		const inputAmount = event.target.value;
+    this.setState({ 
+      input: inputAmount 
+    });
+
+	}
+	onClickHandlingButton= () =>{
+    // const currency = this.state.selectedCurrency;
+		fetch("https://exchangeratesapi.io/api/latest?base=" + this.state.selectedCurrency)
+      .then(data => data.json())
+      .then(response => {
+        const pound = response.rates.GBP;
+        this.setState({ amount: (this.state.input * pound).toFixed(2) });
+      });
+	}
+
 	render() {
-		return (
-			<div className="CalcPayment">
-				<h2 className="CalcPayment-label">Calculate Payment in GBP</h2>
-				<div className="CalcPayment-control">
-					<select onChange={this.selectCurrency} defaultValue={this.state.selectedCurrency}>
-						{this.props.currencies.map((currency, index) => <option key={index}>{currency}</option>)}
-					</select>
-          <input className="CalcPayment-amount" type="text" defaultValue="0.00" />
-          is worth <span className="CalcPayment-result">???</span> in GBP.
+		return <div className="CalcPayment">
+        <h2 className="CalcPayment-label">Calculate Payment in GBP</h2>
+        <div className="CalcPayment-control">
+          <select onChange={this.selectCurrency} defaultValue={this.state.selectedCurrency}>
+            {this.props.currencies.map((currency, index) => (
+              <option key={index}>{currency}</option>
+            ))}
+          </select>
+          <input onChange={this.handleChange} className="CalcPayment-amount" type="text" defaultValue="0.00" />
+          is worth <span className="CalcPayment-result">
+            {this.state.amount}
+          </span> in GBP.
           <div className="CalcPayment-calculate">
-            <Button>Calculate</Button>
+            <Button onClick={this.onClickHandlingButton}>Calculate</Button>
           </div>
-				</div>
-			</div>
-		)
+        </div>
+      </div>;
 	}
 }
 
