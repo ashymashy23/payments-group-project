3e69c1b9538c3a911853658a3b17b157b4321cd9
Abdulkadir <ethiogentelman@gmail.com>
2018-05-31 21:13:14 -0400

    arranging alignment

--- src/components/CalcPayment.js
+++ src/components/CalcPayment.js
@@ -1,60 +1,74 @@
-import React, { Component } from 'react';
-import Button from './Button';
-import './CalcPayment.css';
+import React, { Component } from "react";
+import Button from "./Button";
+import "./CalcPayment.css";
 
 class Payment extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      selectedCurrency: 'USD',
-			amount: 0,
-			input:0
+      selectedCurrency: "USD",
+      amount: 0,
+      input: 0
     };
   }
 
-	selectCurrency = (event) => {
-		const currency = event.target.value;
-		this.setState({
-			selectedCurrency: currency
-		});
-	};
-
-	handleChange=(event) =>{
-		const inputAmount = event.target.value;
-    this.setState({ 
-      input: inputAmount 
+  selectCurrency = event => {
+    const currency = event.target.value;
+    this.setState({
+      selectedCurrency: currency
     });
+  };
 
-	}
-	onClickHandlingButton= () =>{
+  handleChange = event => {
+    const inputAmount = event.target.value;
+    this.setState({
+      input: inputAmount
+    });
+  };
+  onClickHandlingButton = () => {
     // const currency = this.state.selectedCurrency;
-		fetch("https://exchangeratesapi.io/api/latest?base=" + this.state.selectedCurrency)
+    fetch(
+      "https://exchangeratesapi.io/api/latest?base=" +
+        this.state.selectedCurrency
+    )
       .then(data => data.json())
       .then(response => {
         const pound = response.rates.GBP;
-        this.setState({ amount: (this.state.input * pound).toFixed(2) });
+        this.setState({
+          amount: (this.state.input * pound).toFixed(2)
+        });
       });
-	}
+  };
 
-	render() {
-		return <div className="CalcPayment">
+  render() {
+    return (
+      <div className="CalcPayment">
         <h2 className="CalcPayment-label">Calculate Payment in GBP</h2>
         <div className="CalcPayment-control">
-          <select onChange={this.selectCurrency} defaultValue={this.state.selectedCurrency}>
+          <select
+            onChange={this.selectCurrency}
+            defaultValue={this.state.selectedCurrency}
+          >
             {this.props.currencies.map((currency, index) => (
               <option key={index}>{currency}</option>
             ))}
           </select>
-          <input onChange={this.handleChange} className="CalcPayment-amount" type="text" defaultValue="0.00" />
-          is worth <span className="CalcPayment-result">
-            {this.state.amount}
-          </span> in GBP.
+          <input
+            onChange={this.handleChange}
+            className="CalcPayment-amount"
+            type="text"
+            defaultValue="0.00"
+          />
+          is worth{" "}
+          <span className="CalcPayment-result">{this.state.amount}</span> in
+          GBP.
           <div className="CalcPayment-calculate">
             <Button onClick={this.onClickHandlingButton}>Calculate</Button>
           </div>
         </div>
-      </div>;
-	}
+      </div>
+    );
+  }
 }
 
 export default Payment;
